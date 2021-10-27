dc32691d4ab50e385ed68e8c4fead0b764305276
Rakshitha K <91654313+RakshithaK2001@users.noreply.github.com>
2021-10-27 20:49:23 +0530

    Add files via upload

---
+++ d4p2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+  char s[50];
+  printf("Enter string:\n");
+  gets(s);
+  int f[26] = {0};
+  int c = 0;
+  for(int i=0;s[i]!='\0';i++){
+    if(s[i] != ' '){
+      f[s[i]-'a']++;
+      if(f[s[i]-'a'] > f[c])
+        c = s[i]-'a';
+    }
+  }
+
+  printf("%c occurs a maximum of %d times.", c+'a', f[c]);
+  return 0;
+}
