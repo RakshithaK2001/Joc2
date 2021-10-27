b30a222f3ad5c5879dc7a6118d1dedd16c6f9788
Rakshitha K <91654313+RakshithaK2001@users.noreply.github.com>
2021-10-27 20:39:57 +0530

    Add files via upload

---
+++ d4p1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  char s[100];
+  printf("Enter the String:\n");
+  gets(s);
+  printf("The acronym is:");
+  for(int i=0;s[i]!='\0';i++){
+    if(s[i] >= 65 && s[i] <= 90)
+      printf("%c", s[i]);
+    for(;s[i] != ' ';i++);
+  }
+
+  return 0;
+}
